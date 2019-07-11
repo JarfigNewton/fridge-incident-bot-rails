00ef01cac5ecae849a72764775eb4a089aa8804b
Matthew Vo <t.v.tmv@hotmail.com>
2019-07-11 08:11:06 -0700

    Add new command, update puma config and procfile

--- bot/bot.rb
+++ bot/bot.rb
@@ -3,4 +3,9 @@ class Bot < SlackRubyBot::Bot
     IncidentFreeCounter.last.update(days_since_incident: 0)
     client.say(channel: data.channel, text: "The counter has been reset to 0")
   end
+
+  command 'counter' do |client, data, match|
+    counter = IncidentFreeCounter.last.days_since_incident
+    client.say(channel: data.channel, text: "The counter is currently #{counter}")
+  end
 end
