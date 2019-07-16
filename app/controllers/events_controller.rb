89fb5b3d64433f444b7177c7c6586f4daf95d906
Matthew Vo <t.v.tmv@hotmail.com>
2019-07-16 08:58:59 -0700

    Update syntax error

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,9 @@ class EventsController < ApplicationController
   def handle
     # Extract the event payload from the request and parse the JSON
     request_data = JSON.parse(request.body.read)
-    Rails.logger request_data
+    puts '####################################'
+    puts request_data
+    puts '####################################'
     json_message = ""
 
     case request_data['type']
