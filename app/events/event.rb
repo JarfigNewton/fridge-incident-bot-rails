69aae8ef40ba254790f8ec97ae940d696db1f4d4
Matthew Vo <t.v.tmv@hotmail.com>
2019-08-27 13:14:09 -0700

    Update comment

--- app/events/event.rb
+++ app/events/event.rb
@@ -2,7 +2,7 @@ class Event
   def self.message_posted(channel, event_data)
     message = event_data['text']
     return unless message
-    # Don't do anything if message doesn't include fridge and ajar and again
+    # Don't do anything if message doesn't include "fridge" and "ajar"
     return if message.downcase.exclude?("fridge") || message.downcase.exclude?("ajar")
 
     Bot.restart_counter(channel)
